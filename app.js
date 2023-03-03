7beda6f00a99c7ad3ba225a96cd9295405eab2d1
Tiago Gomes <tidgomes@gmail.com>
2023-03-03 16:50:14 -0300

    set a simple server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const feedRoutes = require('./routes/feed')
+
+const app = express()
+
+app.use('/feed', feedRoutes)
+
+app.listen(8080, () => {
+  console.log('Your app is running on port 3000')
+})
---
+++ controllers/feed.js
@@ -0,0 +1,3 @@
+exports.getFeed = (req, res, next) => {
+  res.send('<h1>Hello Express from the controller through the router</h1>')
+}
