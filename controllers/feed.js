eb3461f43ced6b77d18c7dcfd9d50ef5ad11705b
Tiago Gomes <tidgomes@gmail.com>
2023-03-03 17:19:29 -0300

    handle post requests

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,14 @@
 exports.getFeed = (req, res, next) => {
-  res.send('<h1>Hello Express from the controller through the router</h1>')
+  res.status(200).json({ message: 'Olá Tiago!' })
+}
+
+exports.postPost = (req, res, next) => {
+  const [title, content] = [req.body.title, req.body.content]
+
+  res
+    .status(201)
+    .json({
+      message: 'Post created!',
+      post: { date: new Date().toLocaleString(), title, content }
+    })
 }
