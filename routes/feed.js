6b7bb207f92132b642fdac614dbab1792f9bbc5c
Tiago Gomes <tidgomes@gmail.com>
2023-03-03 17:19:50 -0300

    set a post route to /feed/post

--- routes/feed.js
+++ routes/feed.js
@@ -3,5 +3,6 @@ const feedController = require('../controllers/feed')
 const router = express.Router()
 
 router.get('/posts', feedController.getFeed)
+router.post('/post', feedController.postPost)
 
 module.exports = router
