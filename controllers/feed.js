27af440b5a5c7fbe3b6d820f64b2edd9a64f6fa1
Tiago Gomes <tidgomes@gmail.com>
2023-03-04 10:42:39 -0300

    persist received data in the database

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator/check')
+const Post = require('../models/post')
 
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
@@ -22,20 +23,25 @@ exports.createPost = (req, res, next) => {
   const errors = validationResult(req)
 
   if (!errors.isEmpty()) {
-    return res.status(422).json({ message: 'Validation failed', errors: errors.array() })
+    return res
+      .status(422)
+      .json({ message: 'Validation failed', errors: errors.array() })
   }
 
-  res.status(201).json({
-    message: 'Post created!',
-    post: {
-      _id: '321',
-      title,
-      creator: {
-        name: 'Mano'
-      },
-      createdAt: new Date(),
-      content,
-      imageUrl: 'images/pepe.jpg'
-    }
+  const post = new Post({
+    title,
+    content,
+    creator: {
+      name: 'Manolo'
+    },
+    imageUrl: 'images/pepe.jpg'
   })
+
+  post
+    .save()
+    .then(result => {
+      console.log(result)
+      res.status(201).json({ message: 'Post created!', post: result })
+    })
+    .catch(err => console.log(err))
 }
