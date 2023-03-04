0bf539f18add0193d9a53f11a231963e21c1effe
Tiago Gomes <tidgomes@gmail.com>
2023-03-04 10:17:02 -0300

    use dotenv in app file and ignore the .env file

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const feedRoutes = require('./routes/feed')
+
+dotenv.config()
 
 const app = express()
 
