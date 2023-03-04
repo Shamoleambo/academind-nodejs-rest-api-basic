d3ae697d929510b06522147294d12a06fe9b92e9
Tiago Gomes <tidgomes@gmail.com>
2023-03-04 09:17:57 -0300

    set dummy data

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,14 +1,27 @@
 exports.getPosts = (req, res, next) => {
-  res.status(200).json()
+  res
+    .status(200)
+    .json({
+      posts: [
+        {
+          _id: '123',
+          title: 'First Post',
+          creator: {
+            name: 'Mano'
+          },
+          createdAt: new Date(),
+          content: 'Some Content',
+          imageUrl: 'images/pepe.jpg'
+        }
+      ]
+    })
 }
 
 exports.createPost = (req, res, next) => {
   const [title, content] = [req.body.title, req.body.content]
 
-  res
-    .status(201)
-    .json({
-      message: 'Post created!',
-      post: { date: new Date().toLocaleString(), title, content }
-    })
+  res.status(201).json({
+    message: 'Post created!',
+    post: { date: new Date().toLocaleString(), title, content }
+  })
 }
