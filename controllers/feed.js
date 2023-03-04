b43eddc53c9aec28c1d0411951bed76999534716
Tiago Gomes <tidgomes@gmail.com>
2023-03-04 10:03:08 -0300

    add validation for post route

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator/check')
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -16,7 +18,12 @@ exports.getPosts = (req, res, next) => {
 }
 
 exports.createPost = (req, res, next) => {
-  const [title, content] = [req.body.title, req.body.content]
+  const { title, content } = { ...req.body }
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ message: 'Validation failed', errors: errors.array() })
+  }
 
   res.status(201).json({
     message: 'Post created!',
--- routes/feed.js
+++ routes/feed.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const { body } = require('express-validator/check')
 const feedController = require('../controllers/feed')
 const router = express.Router()
 
 router.get('/posts', feedController.getPosts)
-router.post('/post', feedController.createPost)
+router.post(
+  '/post',
+  [
+    body('title').trim().isLength({ min: 5 }).withMessage('Title must have at least 5 characters'),
+    body('content').trim().isLength({ min: 5 }).withMessage('Content must have at least five characters')
+  ],
+  feedController.createPost
+)
 
 module.exports = router
