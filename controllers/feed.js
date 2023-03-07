fb28cd73a87db13141fba972fe1c283493e371a4
Tiago Gomes <tidgomes@gmail.com>
2023-03-07 10:34:23 -0300

    handle edit post requests

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,11 @@
+const fs = require('fs')
+const path = require('path')
 const { validationResult } = require('express-validator/check')
 const Post = require('../models/post')
+
+const clearImage = filePath => {
+  fs.unlink(path.join(__dirname, '..', filePath), err => console.log(err))
+}
 
 exports.getPosts = (req, res, next) => {
   Post.find()
@@ -71,5 +77,54 @@ exports.getPost = (req, res, next) => {
         err.statusCode = 500
       }
       next(err)
+    })
+}
+
+exports.updatePost = (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed.')
+    error.statusCode = 422
+    throw error
+  }
+
+  const postId = req.params.postId
+  const title = req.body.title
+  const content = req.body.content
+  let imageUrl
+
+  if (req.file) {
+    imageUrl = req.file.path.replace('\\', '/')
+  } else imageUrl = req.body.image.replace('\\', '/')
+
+  if (!imageUrl) {
+    const error = new Error('No file picked.')
+    error.statusCode = 422
+    throw error
+  }
+
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('No post found.')
+        error.statusCode = 422
+        throw error
+      }
+
+      if (imageUrl !== post.imageUrl) clearImage(post.imageUrl)
+
+      post.title = title
+      post.content = content
+      post.imageUrl = imageUrl
+
+      return post.save()
+    })
+    .then(result => {
+      res.status(200).json({ message: 'Post updated', post: result })
+    })
+    .catch(err => {
+      const error = new Error(err)
+      error.statusCode = 500
+      next(error)
     })
 }
