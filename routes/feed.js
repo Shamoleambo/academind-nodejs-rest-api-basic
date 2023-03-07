90798a00be4b556f39f03d16d19b5d62c83d7e7c
Tiago Gomes <tidgomes@gmail.com>
2023-03-07 10:34:57 -0300

    create route to edit post and add validations to it

--- routes/feed.js
+++ routes/feed.js
@@ -7,11 +7,31 @@ router.get('/posts', feedController.getPosts)
 router.post(
   '/post',
   [
-    body('title').trim().isLength({ min: 4 }).withMessage('Title must have at least 4 characters'),
-    body('content').trim().isLength({ min: 4 }).withMessage('Content must have at least 4 characters')
+    body('title')
+      .trim()
+      .isLength({ min: 4 })
+      .withMessage('Title must have at least 4 characters'),
+    body('content')
+      .trim()
+      .isLength({ min: 4 })
+      .withMessage('Content must have at least 4 characters')
   ],
   feedController.createPost
 )
 router.get('/post/:postId', feedController.getPost)
+router.put(
+  '/post/:postId',
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 4 })
+      .withMessage('Title must have at least 4 characters'),
+    body('content')
+      .trim()
+      .isLength({ min: 4 })
+      .withMessage('Content must have at least 4 characters')
+  ],
+  feedController.updatePost
+)
 
 module.exports = router
