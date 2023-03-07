819f2f38d0b4af2b9a504607486392234ee4a9a7
Tiago Gomes <tidgomes@gmail.com>
2023-03-07 11:54:25 -0300

    add validation to user request

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,27 @@
 const express = require('express')
+const { body } = require('express-validator/check')
+const User = require('../models/user')
+const authController = require('../controllers/auth')
 
 const router = express.Router()
 
-// router.put('/signup')
+router.put(
+  '/signup',
+  [
+    body('email')
+      .trim()
+      .isEmail()
+      .withMessage('Please enter email')
+      .custom((value, { req }) =>
+        User.find({ email: value }).then(user => {
+          if (user) return Promise.reject('E-mail address already exists')
+        })
+      )
+      .normalizeEmail(),
+    body('name').trim().not().isEmpty(),
+    body('password').trim().notEmpty().isLength({ min: 5 })
+  ],
+  authController.signup
+)
 
 module.exports = router
