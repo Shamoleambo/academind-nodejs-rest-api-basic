803125b67a80e3beedcfd64b8beef8d444480893
Tiago Gomes <tidgomes@gmail.com>
2023-03-07 11:55:00 -0300

    create function to handle user signup and check for validation errors

---
+++ controllers/auth.js
@@ -0,0 +1,13 @@
+const { validationResult } = require('express-validator/check')
+const User = require('../models/user')
+
+exports.signup = (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation Failed')
+    error.statusCode = 422
+    error.data = errors.array()
+    throw error
+  }
+  const { name, email, password } = { ...req.body }
+}
