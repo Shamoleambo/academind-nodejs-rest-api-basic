99b774953b581a95e3f6f3148499e202ef28cb8e
Tiago Gomes <tidgomes@gmail.com>
2023-03-07 14:04:10 -0300

    attach a token to the newly logged in user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,10 @@
 const { validationResult } = require('express-validator/check')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
 const User = require('../models/user')
+
+dotenv.config()
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req)
@@ -20,6 +24,45 @@ exports.signup = (req, res, next) => {
     })
     .then(result => {
       res.status(201).json({ message: 'User created!', userId: result._id })
+    })
+    .catch(err => {
+      if (!err.statusCode) err.statusCode = 500
+      next(err)
+    })
+}
+
+exports.login = (req, res, next) => {
+  const { email, password } = { ...req.body }
+  let loadedUser
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        const error = new Error('No user with such email')
+        error.statusCode = 401
+        throw error
+      }
+
+      loadedUser = user
+      return bcrypt.compare(password, user.password)
+    })
+    .then(isEqual => {
+      if (!isEqual) {
+        const error = new Error('Wrong password')
+        error.statusCode = 401
+        throw error
+      }
+
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString()
+        },
+        `${process.env.JWT_SECRET}`,
+        { expiresIn: '1h' }
+      )
+
+      res.status(200).json({ token, userId: loadedUser._id.toString() })
     })
     .catch(err => {
       if (!err.statusCode) err.statusCode = 500
