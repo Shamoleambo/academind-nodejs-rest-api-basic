a2ecb2b10e16d8376e794a07f31e4f86d31dd0b3
Tiago Gomes <tidgomes@gmail.com>
2023-03-07 15:15:46 -0300

    refactor user model to be accurate

--- models/post.js
+++ models/post.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const postSchema = new Schema(
   {
     title: { type: String, required: true },
-    creator: { type: Object, required: true },
+    creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     content: { type: String, required: true },
     imageUrl: { type: String, required: true }
   },
