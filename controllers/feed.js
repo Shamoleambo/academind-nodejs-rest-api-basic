5dfefc010451869be990ac0913fc56bb2e8a8db5
Tiago Gomes <tidgomes@gmail.com>
2023-03-08 10:46:19 -0300

    handle update status requests

--- controllers/feed.js
+++ controllers/feed.js
@@ -190,3 +190,28 @@ exports.deletePost = (req, res, next) => {
       next(err)
     })
 }
+
+exports.updateStatus = (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    const error = new Error('Invalid status field')
+    error.statusCode = 422
+    throw error
+  }
+
+  const status = req.body.status
+
+  User.findById(req.userId)
+    .then(user => {
+      user.status = status
+      return user.save()
+    })
+    .then(() => {
+      console.log('Status updated')
+      res.status(200).json({ message: 'User status updated!' })
+    })
+    .catch(err => {
+      if (!err.statusCode) err.statusCode = 500
+      next(err)
+    })
+}
