f503fbd65737b14b1ce6bbff05af235bad295708
Tiago Gomes <tidgomes@gmail.com>
2023-03-09 11:38:42 -0300

    create resolver for login query

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,10 @@
 const bcrypt = require('bcrypt')
 const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
 const User = require('../models/user')
+
+dotenv.config()
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
@@ -35,5 +39,31 @@ module.exports = {
     const createdUser = await user.save()
 
     return { ...createdUser._doc, _id: createdUser._id.toString() }
+  },
+  login: async ({ email, password }, req) => {
+    const user = await User.findOne({ email })
+    if (!user) {
+      const error = new Error('User not found')
+      error.code = 401
+      throw error
+    }
+
+    const passwordsMatch = await bcrypt.compare(password, user.password)
+    if (!passwordsMatch) {
+      const error = new Error('Wrong password')
+      error.code = 401
+      throw error
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        email: user.email
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    )
+
+    return { token, userId: user._id.toString() }
   }
 }
